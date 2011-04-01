9fdf56ae6f9cfefeb540da525620bd314adfa8c6
Thomas Roger <troger@nuxeo.com>
2011-04-01 15:44:12 +0200

    add current Workspace, Domain and SuperSpace id as contextual parameters

--- nuxeo-birt-reporting/src/main/java/org/nuxeo/ecm/platform/reporting/report/ReportContext.java
+++ nuxeo-birt-reporting/src/main/java/org/nuxeo/ecm/platform/reporting/report/ReportContext.java
@@ -20,7 +20,7 @@ package org.nuxeo.ecm.platform.reporting.report;
 
 import java.util.List;
 
-import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.ClientException;import org.nuxeo.ecm.core.api.DocumentModel;
 
 /**
  * Holds the logic to extract parameters from the Document Context.
@@ -30,21 +30,37 @@ import org.nuxeo.ecm.core.api.DocumentModel;
  */
 public class ReportContext {
 
-    public static String USER_NAME = "userName";
+    public static final String DOMAIN_DOCUMENT_TYPE = "Domain";
 
-    public static String DOC_TYPE = "docType";
+    public static final String WORKSPACE_DOCUMENT_TYPE = "Workspace";
 
-    public static String CURRENT_PATH = "currentPath";
+    public static final String USER_NAME = "userName";
 
-    public static String CURRENT_REPOSITORY = "currentRepository";
+    public static final String DOC_TYPE = "docType";
 
-    public static String CURRENT_WORKSPACE_PATH = "currentWorkspacePath";
+    public static final String CURRENT_PATH = "currentPath";
 
-    public static String CURRENT_DOMAIN_PATH = "currentDomainPath";
+    public static final String CURRENT_REPOSITORY = "currentRepository";
+
+    public static final String CURRENT_SUPERSPACE_PATH = "currentSuperspacePath";
+
+    public static final String CURRENT_SUPERSPACE_ID = "currentSuperspaceId";
+
+    public static final String CURRENT_WORKSPACE_PATH = "currentWorkspacePath";
+
+    public static final String CURRENT_WORKSPACE_ID = "currentWorkspaceId";
+
+    public static final String CURRENT_DOMAIN_PATH = "currentDomainPath";
+
+    public static final String CURRENT_DOMAIN_ID = "currentDomainPath";
 
     public static void setContextualParameters(
             List<ReportParameter> reportParams, DocumentModel doc)
             throws Exception {
+        DocumentModel currentDomain = getFirstParentWithType(doc, DOMAIN_DOCUMENT_TYPE);
+        DocumentModel currentWorkspace = getFirstParentWithType(doc, WORKSPACE_DOCUMENT_TYPE);
+        DocumentModel currentSuperSpace = getCurrentSuperSpace(doc);
+
         for (ReportParameter param : reportParams) {
             if (param.getName().equals(USER_NAME)) {
                 param.setValue(doc.getCoreSession().getPrincipal().getName());
@@ -54,26 +70,47 @@ public class ReportContext {
                 param.setValue(doc.getPathAsString());
             } else if (param.getName().equals(CURRENT_REPOSITORY)) {
                 param.setValue(doc.getRepositoryName());
-            } else if (param.getName().equals(CURRENT_WORKSPACE_PATH)) {
-                List<DocumentModel> parents = doc.getCoreSession().getParentDocuments(
-                        doc.getRef());
-                for (DocumentModel parent : parents) {
-                    if (parent.getType().equals("Workspace")) {
-                        param.setValue(parent.getPathAsString());
-                        break;
-                    }
-                }
             } else if (param.getName().equals(CURRENT_DOMAIN_PATH)) {
-                List<DocumentModel> parents = doc.getCoreSession().getParentDocuments(
-                        doc.getRef());
-                for (DocumentModel parent : parents) {
-                    if (parent.getType().equals("Domain")) {
-                        param.setValue(parent.getPathAsString());
-                        break;
-                    }
+                if (currentDomain != null) {
+                    param.setValue(currentDomain.getPathAsString());
+                }
+            } else if (param.getName().equals(CURRENT_DOMAIN_ID)) {
+                if (currentDomain != null) {
+                    param.setValue(currentDomain.getId());
+                }
+            } else if (param.getName().equals(CURRENT_WORKSPACE_PATH)) {
+                if (currentWorkspace != null) {
+                    param.setValue(currentWorkspace.getPathAsString());
+                }
+            } else if (param.getName().equals(CURRENT_WORKSPACE_ID)) {
+                if (currentWorkspace != null) {
+                    param.setValue(currentWorkspace.getId());
+                }
+            } else if (param.getName().equals(CURRENT_SUPERSPACE_PATH)) {
+                if (currentSuperSpace != null) {
+                    param.setValue(currentSuperSpace.getPathAsString());
+                }
+            } else if (param.getName().equals(CURRENT_SUPERSPACE_ID)) {
+                if (currentSuperSpace != null) {
+                    param.setValue(currentSuperSpace.getId());
                 }
             }
         }
+    }
+
+    private static DocumentModel getFirstParentWithType(DocumentModel doc, String type) throws ClientException {
+        List<DocumentModel> parents = doc.getCoreSession().getParentDocuments(
+                doc.getRef());
+        for (DocumentModel parent : parents) {
+            if (parent.getType().equals(type)) {
+                return parent;
+            }
+        }
+        return null;
+    }
+
+    private static DocumentModel getCurrentSuperSpace(DocumentModel doc) throws ClientException {
+        return doc.getCoreSession().getSuperSpace(doc);
     }
 
 }

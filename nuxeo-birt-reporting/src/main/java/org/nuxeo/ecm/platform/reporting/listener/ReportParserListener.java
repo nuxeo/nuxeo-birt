3873db7265fdeed9c4e4f9ff4c4a77725f3c4d02
Florent Guillaume <fg@nuxeo.com>
2015-07-09 17:36:01 +0200

    NXP-17449: move some exceptions

--- nuxeo-birt-reporting/src/main/java/org/nuxeo/ecm/platform/reporting/listener/ReportParserListener.java
+++ nuxeo-birt-reporting/src/main/java/org/nuxeo/ecm/platform/reporting/listener/ReportParserListener.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.model.PropertyException;
+import org.nuxeo.ecm.core.api.PropertyException;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
